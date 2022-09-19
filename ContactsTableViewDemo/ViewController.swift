00397373d189a8fc9b9a68a229d70dd47209225d
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-19 20:34:39 +0300

    configure cells

--- ContactsTableViewDemo/ViewController.swift
+++ ContactsTableViewDemo/ViewController.swift
@@ -25,15 +25,25 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // получаем экземпляр ячейки
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
         
-        // конфигурируем ячейку
+        var cell: UITableViewCell
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "MyCell") {
+            print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+            cell = reuseCell
+        } else {
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            cell =  UITableViewCell(style: .default,
+                                    reuseIdentifier: "MyCell")
+        }
+
+        configure(cell: &cell, for: indexPath)
+        return cell
+    }
+    
+    private func configure(cell: inout UITableViewCell,
+                           for indexPath: IndexPath) {
         var configuration = cell.defaultContentConfiguration()
         configuration.text = "Строка \(indexPath.row)"
         cell.contentConfiguration = configuration
-        
-        // возвращаем сконфигурированный экземпляр ячейки
-        return cell
     }
 }
